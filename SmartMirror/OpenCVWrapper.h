06e6d47dcf9bf363755b820e06102decdb81009c
SunnySYChen <sunny.chen.shiyao@gmail.com>
2020-01-01 16:44:51 -0500

    Add OpenCV framework

---
+++ SmartMirror/OpenCVWrapper.h
@@ -0,0 +1,19 @@
+//
+//  OpenCVWrapper.h
+//  SmartMirror
+//
+//  Created by Sunny Chen on 2020-01-01.
+//  Copyright © 2020 Team 2019053. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface OpenCVWrapper : NSObject
+
++ (NSString *)openCVVersionString;
+
+@end
+
+NS_ASSUME_NONNULL_END
